1ea91d5a4b09a2e646e4d5371a5c882a04f3f5bc
Luis Espinola <lespinola@gmail.com>
2018-11-29 10:37:43 -0500

    initial commit

---
+++ public/js/main2.js
@@ -0,0 +1,24 @@
+//event listeners.
+document.addEventListener('DOMContentLoaded', () => {
+    if (typeof fin != 'undefined') {
+        fin.desktop.main(onMain);
+    } else {
+        ofVersion.innerText =
+            'OpenFin is not available - you are probably running in a browser.';
+    }
+});
+
+//once the DOM has loaded and the OpenFin API is ready
+function onMain() {
+    //get a reference to the current Application.
+    const app = fin.desktop.Application.getCurrent();
+
+    //we will increment on child window creation.
+    let winNumber = 0;
+
+    //we get the current OpenFin version
+    fin.desktop.System.getVersion(version => {
+        const ofVersion = document.querySelector('#of-version');
+        ofVersion.innerText = version;
+    });
+}
